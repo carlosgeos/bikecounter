ec67501c0abaec9e0a177dd888182859b845d369
Carlos Requena López <crequena@ulb.ac.be>
2019-10-07 22:46:12 +0200

    Add clojure.spec model for GeoJSON resource

--- src/bikecounter/fetch.clj
+++ src/bikecounter/fetch.clj
@@ -1,13 +1,16 @@
 (ns bikecounter.fetch
-  (:require [clojure.java.jdbc :as j]
-            [environ.core :refer [env]]
-            [clj-http.client :as requests]
+  (:gen-class
+   :methods [^:static [handler [String] String]])
+  (:require [bikecounter.common :refer [db]]
             [cheshire.core :as json]
-            [hugsql.core :as hugsql]
-            [bikecounter.common :refer [db]])
-  (:import [com.sendgrid SendGrid Request]
-           [com.sendgrid.helpers.mail Mail]
-           [com.sendgrid.helpers.mail.objects Email Content]))
+            [clj-http.client :as requests]
+            [clojure.java.jdbc :as j]
+            [clojure.spec.alpha :as s]
+            [environ.core :refer [env]]
+            [hugsql.core :as hugsql])
+  (:import [com.sendgrid SendGrid Request Method]
+           com.sendgrid.helpers.mail.Mail
+           [com.sendgrid.helpers.mail.objects Content Email]))
 
 ;; ;; The path is relative to the classpath (not proj dir!), so "src" is
 ;; ;; not included in the path.  The same would apply if the sql was
@@ -18,43 +21,47 @@
 
 (def RESOURCE_URL "http://data-mobility.brussels/geoserver/bm_bike/wfs?service=wfs&version=1.1.0&request=GetFeature&typeName=bm_bike:rt_counting&outputFormat=json")
 
-;; (defn notify-of-failure
-;;   "Sends an email using SendGrid's API, saying that something went wrong
-;;   with the bike counter resource"
-;;   []
-;;   (def sg (SendGrid. (env :sendgrid-api-key)))
-;;   (def email (Mail. (Email. "bikecounter@herokuapps.com") ;from
-;;                     "[bikecounter] Failure notification"  ;subject
-;;                     (Email. (env :email))                 ;to
-;;                     (Content. "text/plain" (str "Cron job failed to fetch the remote GeoJSON resource on "
-;;                                                 (tf/unparse (tf/formatter :rfc822) (t/now))))))
+(defn notify-of-failure
+  "Sends an email using SendGrid's API, saying that something went wrong
+  with the bike counter resource"
+  []
+  (def sg (SendGrid. (env :sendgrid-api-key)))
+  (def email (Mail. (Email. "bikecounter@lambda.com") ;from
+                    "[bikecounter] Failure notification"  ;subject
+                    (Email. (env :email))                 ;to
+                    (Content. "text/plain" "Cron job failed to fetch the remote GeoJSON resource")))
 
-;;   (def req (Request.))
-;;   (.setMethod req Method/POST)
-;;   (.setEndpoint req "mail/send")
-;;   (.setBody req (.build email))
-;;   (.api sg req))
-
-
-(def raw-data
-  (try
-    (as-> (json/parse-string (:body (requests/get RESOURCE_URL))) obj
-      (get obj "features"))
-    ;; (filter #(= (get (get % "properties") "id") CORRECT_ID) obj)
-    ;; (first obj)
-    ;; (get obj "properties"))
-
-    (catch Exception e
-      ;; (notify-of-failure)
-      )))
-
-(println raw-data)
+  (def req (Request.))
+  (.setMethod req Method/POST)
+  (.setEndpoint req "mail/send")
+  (.setBody req (.build email))
+  (.api sg req))
 
 
+(defn fetch-data
+  ""
+  []
+  (json/parse-string (:body (requests/get RESOURCE_URL)) true))
 
-;; ;; If the above fails, then everything fails since we can't continue
-;; ;; with a SendGrid Response as raw-data.
+(def result (fetch-data))
 
+(s/def :property/id int?)
+(s/def ::properties
+  (s/keys :req-un [::road_en ::hour_cnt ::day_cnt ::year_cnt ::cnt_time]
+          :opt-un [::device_name :property/id]))
+(s/def :feature/id string?)
+(s/def :feature/type #{"Feature"})
+(s/def ::feature
+  (s/keys :req-un [::properties]
+          :opt-un [:feature/id :feature/type]))
+(s/def :root/type #{"FeatureCollection"})
+(s/def ::totalFeatures pos-int?)
+(s/def ::features (s/coll-of ::feature))
+(s/def ::geojson-spec
+  (s/keys :req-un [:root/type ::totalFeatures ::features]))
+
+(when (s/valid? ::geojson-spec result)
+  (notify-of-failure))
 
 ;; (def bikers-currently {:ts      (tf/parse (tf/formatter :date-time-no-ms)
 ;;                                           (raw-data "cnt_time"))
